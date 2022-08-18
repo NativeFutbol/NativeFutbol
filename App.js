346b46c59ba41f7bb15770da9f4ea3688720da2e
Fei Li <feili4287@gmail.com>
2022-08-18 16:20:16 -0400

    added filters component

--- App.js
+++ App.js
@@ -3,13 +3,20 @@ import { SafeAreaView, StyleSheet, Text, View } from "react-native";
 import CustomSearchBar from "./components/CustomSearchBar";
 import AllCountriesScreen from "./screens/AllCountriesScreen";
 import { FOOTBALL_API_KEY } from "@env";
+import { NavigationContainer } from "@react-navigation/native";
+import { createNativeStackNavigator } from "@react-navigation/native-stack";
 
 export default function App() {
-  console.log(FOOTBALL_API_KEY);
+  const Stack = createNativeStackNavigator();
+  const screenOptions = {
+    headerShown: false,
+  };
   return (
-    <View>
-      <AllCountriesScreen />
-    </View>
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={screenOptions}>
+        <Stack.Screen name="AllCountries" component={AllCountriesScreen} />
+      </Stack.Navigator>
+    </NavigationContainer>
   );
 }
 
--- screens/AllCountriesScreen.js
+++ screens/AllCountriesScreen.js
@@ -3,12 +3,14 @@ import React, { useState } from "react";
 import CustomSearchBar from "../components/CustomSearchBar";
 import axios from "axios";
 import { FOOTBALL_API_KEY } from "@env";
+import Filters from "../components/Filters";
 
 export default function AllCountriesScreen() {
   const [query, setQuery] = useState("");
   const [filter, setFilter] = useState("countries");
 
   const getCountries = () => {
+    console.log("allcoun", FOOTBALL_API_KEY);
     const options = {
       method: "GET",
       url: `https://v3.football.api-sports.io/${filter}?search=${query}`,
@@ -30,14 +32,13 @@ export default function AllCountriesScreen() {
 
   return (
     <View>
-      <SafeAreaView>
-        <CustomSearchBar
-          query={query}
-          setQuery={setQuery}
-          placeholder="Search countries..."
-          onSubmit={getCountries}
-        />
-      </SafeAreaView>
+      <CustomSearchBar
+        query={query}
+        setQuery={setQuery}
+        placeholder="Search countries..."
+        onSubmit={getCountries}
+      />
+      <Filters />
     </View>
   );
 }
