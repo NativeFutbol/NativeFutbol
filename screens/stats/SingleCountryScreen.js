acebb37e59d4bbbcff68494dc2ee0c2535de736a
Kevin <kevinliang327@gmail.com>
2022-08-23 15:42:51 -0400

    correct league shows on single country

--- screens/stats/SingleCountryScreen.js
+++ screens/stats/SingleCountryScreen.js
@@ -1,29 +1,36 @@
 import axios from "axios";
 import { FOOTBALL_API_KEY } from "@env";
 import { View, Text, SafeAreaView, StyleSheet } from "react-native";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 
 import CustomSearchBar from "../../components/CustomSearchBar";
 import Filters from "../../components/Filters";
 import SeasonFilter from "../../components/SeasonFilter";
 import CategoryList from "../../components/CategoryList";
+import LoadingOverlay from "../../components/LoadingOverlay";
+import { useSelector } from "react-redux";
 
 export default function AllCountriesScreen() {
   const [query, setQuery] = useState("");
   const [season, setSeason] = useState("2022");
-  const [filter, setFilter] = useState("countries");
-  const [countries, setCountries] = useState([
-    "England",
-    "France",
-    "Germany",
-    "Italy",
-    "Spain",
-  ]);
+  const [filter, setFilter] = useState("leagues");
+  const [leagueData, setLeagueData] = useState([]);
+  const [isLoading, setIsLoading] = useState(false);
 
-  const getCountries = () => {
+  const singleCountryData = useSelector((state) => state.singleScreenData);
+
+  const topFiveLeagues = [61, 135, 78, 140, 39];
+
+  useEffect(() => {
+    getLeagues();
+  }, []);
+
+  const getLeagues = () => {
+    setIsLoading(true);
+
     const searchUrl =
       query === ""
-        ? `https://v3.football.api-sports.io/${filter}`
+        ? `https://v3.football.api-sports.io/${filter}?season=${season}`
         : `https://v3.football.api-sports.io/${filter}?search=${query}`;
 
     const options = {
@@ -38,18 +45,23 @@ export default function AllCountriesScreen() {
     axios
       .request(options)
       .then(function (response) {
-        console.log(
-          response.data.response.filter((country) =>
-            countries.includes(country.name)
-          )
+        const leagues = response.data.response.filter(
+          (league) =>
+            topFiveLeagues.includes(league.league.id) &&
+            singleCountryData.country.name === league.country.name
         );
+
+        setLeagueData(leagues);
+        setIsLoading(false);
       })
       .catch(function (error) {
         console.error(error);
       });
   };
 
-  console.log(country);
+  if (isLoading) {
+    return <LoadingOverlay />;
+  }
 
   return (
     <SafeAreaView>
@@ -58,19 +70,13 @@ export default function AllCountriesScreen() {
           query={query}
           setQuery={setQuery}
           placeholder="Search countries..."
-          onSubmit={getCountries}
+          onSubmit={getLeagues}
         />
         <Filters />
         <SeasonFilter season={season} setSeason={setSeason} />
       </View>
-      {/* <View style={styles.imageContainer}>
-        {league.logo === "" ? (
-          <></>
-        ) : (
-          <Image source={{ uri: league.logo, width: 100, height: 100 }} />
-        )}
-        <Text>{league.name}</Text>
-      </View> */}
+
+      <CategoryList data={leagueData} filter={filter} />
     </SafeAreaView>
   );
 }
