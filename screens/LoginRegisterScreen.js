4ffd5d0fec022776064c357bf808db47925dc6e2
Kevin <kevinliang327@gmail.com>
2022-08-24 12:38:05 -0400

    added animated list to login/register

--- screens/LoginRegisterScreen.js
+++ screens/LoginRegisterScreen.js
@@ -5,15 +5,103 @@ import {
   TextInput,
   TouchableOpacity,
   View,
+  Dimensions,
+  FlatList,
+  Animated,
 } from "react-native";
-import React, { useEffect, useState } from "react";
+import React, { useEffect, useRef, useState } from "react";
 import { SafeAreaView } from "react-native-safe-area-context";
 import { auth } from "../firebase";
 import { useNavigation } from "@react-navigation/native";
 
+const { width, height } = Dimensions.get("screen");
+const data = [
+  { name: "Login", key: 1, ref: React.createRef() },
+  { name: "Register", key: 2, ref: React.createRef() },
+];
+
+const Tab = React.forwardRef(({ item, onItemPress }, ref) => {
+  return (
+    <TouchableOpacity onPress={onItemPress}>
+      <View ref={ref}>
+        <Text style={{ fontSize: 20, fontWeight: "600" }}>{item.name}</Text>
+      </View>
+    </TouchableOpacity>
+  );
+});
+
+const Indicator = ({ measures, scrollX }) => {
+  const inputRange = data.map((_, i) => i * width);
+  const indicatorWidth = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.width),
+  });
+  const translateX = scrollX.interpolate({
+    inputRange,
+    outputRange: measures.map((measure) => measure.x),
+  });
+
+  return (
+    <Animated.View
+      style={{
+        position: "absolute",
+        height: 4,
+        width: indicatorWidth,
+        left: 0,
+        backgroundColor: "black",
+        bottom: -10,
+        transform: [
+          {
+            translateX,
+          },
+        ],
+      }}
+    ></Animated.View>
+  );
+};
+
+const Tabs = ({ data, scrollX, onItemPress }) => {
+  const measures = [
+    { height: 24, width: 50.333343505859375, x: 95.66666412353516, y: 0 },
+    { height: 24, width: 76.6666259765625, x: 241.6666717529297, y: 0 },
+  ];
+
+  return (
+    <View style={{ position: "absolute", top: 10, width }}>
+      <View
+        style={{
+          justifyContent: "space-evenly",
+          flex: 1,
+          flexDirection: "row",
+        }}
+      >
+        {data.map((item, index) => {
+          return (
+            <Tab
+              key={item.key}
+              item={item}
+              ref={item.ref}
+              onItemPress={() => onItemPress(index)}
+            />
+          );
+        })}
+      </View>
+      <Indicator measures={measures} scrollX={scrollX} />
+    </View>
+  );
+};
+
 export default function LoginRegister() {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const ref = useRef();
+
+  const scrollX = React.useRef(new Animated.Value(0)).current;
+  const onItemPress = React.useCallback((itemIndex) => {
+    ref?.current?.scrollToOffset({
+      offset: itemIndex * width,
+    });
+  });
 
   const handleSignUp = () => {
     auth
@@ -36,36 +124,54 @@ export default function LoginRegister() {
   };
 
   return (
-    <KeyboardAvoidingView style={styles.container} behavior="padding">
-      <View style={styles.inputContainer}>
-        <TextInput
-          placeholder="Email"
-          value={email}
-          onChangeText={(text) => setEmail(text)}
-          style={styles.input}
-        />
-        <TextInput
-          placeholder="Password"
-          value={password}
-          onChangeText={(text) => setPassword(text)}
-          style={styles.input}
-          secureTextEntry
-        />
-      </View>
+    <Animated.FlatList
+      ref={ref}
+      data={data}
+      keyExtractor={(item) => item.key}
+      horizontal
+      showsHorizontalScrollIndicator={false}
+      pagingEnabled
+      bounces={false}
+      onScroll={Animated.event(
+        [{ nativeEvent: { contentOffset: { x: scrollX } } }],
+        { useNativeDriver: false }
+      )}
+      renderItem={({ item }) => {
+        return (
+          <KeyboardAvoidingView style={styles.container} behavior="padding">
+            <Tabs scrollX={scrollX} data={data} onItemPress={onItemPress} />
+            <View style={styles.inputContainer}>
+              <TextInput
+                placeholder="Email"
+                value={email}
+                onChangeText={(text) => setEmail(text)}
+                style={styles.input}
+              />
+              <TextInput
+                placeholder="Password"
+                value={password}
+                onChangeText={(text) => setPassword(text)}
+                style={styles.input}
+                secureTextEntry
+              />
+            </View>
 
-      <View style={styles.buttonContainer}>
-        <TouchableOpacity onPress={handleLogin} style={styles.button}>
-          <Text style={styles.buttonText}>Login</Text>
-        </TouchableOpacity>
+            <View style={styles.buttonContainer}>
+              <TouchableOpacity onPress={handleLogin} style={styles.button}>
+                <Text style={styles.buttonText}>Login</Text>
+              </TouchableOpacity>
 
-        <TouchableOpacity
-          onPress={handleSignUp}
-          style={[styles.button, styles.buttonOutline]}
-        >
-          <Text style={styles.buttonOutlineText}>Register</Text>
-        </TouchableOpacity>
-      </View>
-    </KeyboardAvoidingView>
+              <TouchableOpacity
+                onPress={handleSignUp}
+                style={[styles.button, styles.buttonOutline]}
+              >
+                <Text style={styles.buttonOutlineText}>Register</Text>
+              </TouchableOpacity>
+            </View>
+          </KeyboardAvoidingView>
+        );
+      }}
+    />
   );
 }
 
@@ -74,6 +180,8 @@ const styles = StyleSheet.create({
     flex: 1,
     justifyContent: "center",
     alignItems: "center",
+    width,
+    height,
   },
   inputContainer: { width: "80%" },
   input: {
