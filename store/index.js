98cce94412c22c0577f0072d1213b8d9b1c9fa83
Connor Choi <choic8516@gmail.com>
2022-08-30 17:27:36 -0400

    async storage for my teams

--- store/index.js
+++ store/index.js
@@ -10,17 +10,81 @@ import myTeamFilterOptionsReducer from "./myTeamFilterOptions";
 import myPlayersReducer from "./myPlayers";
 import myFormationReducer from "./myFormation";
 
-export const store = configureStore({
-  reducer: {
-    singleScreenData: singleScreenDataReducer,
-    standingsData: standingsDataReducer,
-    season: seasonReducer,
-    topScorersData: topScorersReducer,
-    topAssistsData: topAssistsReducer,
-    mostCardsData: mostCardsReducer,
-    myTeamFilters: myTeamFiltersReducer,
-    myTeamFilterOptions: myTeamFilterOptionsReducer,
-    myPlayers: myPlayersReducer,
-    myFormation: myFormationReducer,
-  },
+import storage from "redux-persist/lib/storage";
+// import storage from "redux-persist/es/storage";
+// import AsyncStorage from "@react-native-async-storage/async-storage";
+// import { AsyncStorage } from "react-native";
+// import AsyncStorage from "@react-native-community/async-storage";
+
+import {
+  persistStore,
+  persistReducer,
+  FLUSH,
+  REHYDRATE,
+  PAUSE,
+  PERSIST,
+  PURGE,
+  REGISTER,
+} from "redux-persist";
+import { combineReducers } from "redux";
+
+const persistConfig = {
+  key: "root",
+  version: 1,
+  blacklist: [
+    "singleScreenData",
+    "standingsData",
+    "season",
+    "topScorersData",
+    "topAssistsData",
+    "mostCardsData",
+    "myTeamFilters",
+    "myTeamFilterOptions",
+  ],
+  storage,
+  // storage,
+};
+
+const reducer = combineReducers({
+  singleScreenData: singleScreenDataReducer,
+  standingsData: standingsDataReducer,
+  season: seasonReducer,
+  topScorersData: topScorersReducer,
+  topAssistsData: topAssistsReducer,
+  mostCardsData: mostCardsReducer,
+  myTeamFilters: myTeamFiltersReducer,
+  myTeamFilterOptions: myTeamFilterOptionsReducer,
+
+  myPlayers: myPlayersReducer,
+  myFormation: myFormationReducer,
 });
+
+const persistedReducer = persistReducer(persistConfig, reducer);
+
+export const store = configureStore({
+  reducer: persistedReducer,
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware({
+      serializableCheck: {
+        ignoredActions: [FLUSH, REHYDRATE, PAUSE, PERSIST, PURGE, REGISTER],
+      },
+    }),
+});
+
+export const persistor = persistStore(store);
+
+// export const store = configureStore({
+//   reducer: {
+//     singleScreenData: singleScreenDataReducer,
+//     standingsData: standingsDataReducer,
+//     season: seasonReducer,
+//     topScorersData: topScorersReducer,
+//     topAssistsData: topAssistsReducer,
+//     mostCardsData: mostCardsReducer,
+//     myTeamFilters: myTeamFiltersReducer,
+//     myTeamFilterOptions: myTeamFilterOptionsReducer,
+//     myPlayers: myPlayersReducer,
+//     myFormation: myFormationReducer,
+//     persistedReducer: persistedReducer,
+//   },
+// });
